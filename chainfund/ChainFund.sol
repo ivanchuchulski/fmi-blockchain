643d40814b086d55d16a34c956a898842c2eb978
Ivan Chuchulski <zake6@abv.bg>
2021-01-25 12:19:50 +0200

    fix finished causes, add view of donators for cause

--- chainfund/ChainFund.sol
+++ chainfund/ChainFund.sol
@@ -1,94 +1,118 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.6.6 <0.8.0;
+pragma experimental ABIEncoderV2;
 
 import "./Cause.sol";
 import "./Donation.sol";
 
 contract ChainFund {
-    uint256 private numCauses;
-    mapping(string => Cause) private causes;
     string[] private registeredCauses;
     string[] private finishedCauses;
 
-    mapping (string => Donation[]) causeDonators;
+    mapping(string => Cause) private causes;
+    mapping(string => Donation[]) private causeDonators;
 
     constructor() {
-        numCauses = 0;
     }
 
-    function addCause(address payable beneficiary, uint256 goal, string memory description) public {
-        require(!isCauseRegistered(description), "error : cause already registered");
-        require(isCauseFinished(description), "error : cause already finished");
+    function addCause(address payable beneficiary, uint256 goal, string memory title) public {
+        require(!isCauseRegistered(title), "error : cause already registered");
 
-        registeredCauses.push(description);
-        causes[description] = Cause(description, beneficiary, goal, 0);
+        registeredCauses.push(title);
+        causes[title] = Cause(title, beneficiary, goal, 0);
     }
 
-    function donateToCause(string memory description) public payable {
-        require(isCauseRegistered(description), "error : not such cause registered");
-        require(!isCauseFinished(description), "error : cause already finished");
-    
-        Cause storage cause = causes[description];
+    function donateToCause(string memory title) public payable {
+        require(isCauseRegistered(title), "error : not such cause registered");
+        require(!isCauseFinished(title), "error : cause already finished");
+
+        Cause storage cause = causes[title];
         address sender = msg.sender;
         uint256 value = msg.value;
 
-        Donation[] storage donatorsForCause = causeDonators[cause.description];
+        Donation[] storage donatorsForCause = causeDonators[cause.title];
 
         cause.currentAmount += value;
         donatorsForCause.push(Donation(sender, value, block.timestamp));
 
         if (checkGoalReached(cause)) {
             transferCauseDonations(cause);
-            
-            // TODO : handle finished causes
-            finishedCauses.push(cause.description);
+            finishedCauses.push(cause.title);
         }
     }
 
-    function isCauseGoalReached(string memory description) public view returns (bool reached) {
-        require(isCauseRegistered(description), "error : not such cause registered");
+    function isCauseGoalReached(string memory title) public view returns (bool reached) {
+        require(isCauseRegistered(title), "error : not such cause registered");
 
-        Cause storage cause = causes[description];
+        Cause storage cause = causes[title];
 
-        return cause.currentAmount >= cause.goal;
+        return checkGoalReached(cause);
     }
 
-    function checkCauseProgress(string memory description) public view returns (uint) {
-        require(isCauseRegistered(description), "error : not such cause registered");
+    function checkCauseProgress(string memory title) public view returns (uint) {
+        require(isCauseRegistered(title), "error : not such cause registered");
 
-        Cause storage cause = causes[description];
+        Cause storage cause = causes[title];
 
         return cause.currentAmount;
     }
 
-    // this does not work on like that, we can either destrcutively return the donations or use experimental feature
-    // function getDonationsForCause(string memory description) public view returns (Donation[] memory) {
-        
-    // }
+    function getNumberOfRegisteredCauses() public view returns (uint256) {
+        return registeredCauses.length;
+    }
+
+    function getNumberOfFinishedCauses() public view returns (uint256) {
+        return finishedCauses.length;
+    }
+
+    // uses the experimental feature
+    function getDonationsForCauseExperimental(string memory title) public view returns (Donation[] memory) {
+        require(isCauseRegistered(title), "error : not such cause registered");
+
+        return causeDonators[title];
+    }
+
+    function getDonationsForCause(string memory title) public view returns (address[] memory, uint256[] memory, uint256[] memory) {
+        require(isCauseRegistered(title), "error : not such cause registered");
+
+        Donation[] storage donationsForCause = causeDonators[title];
+
+        address[] memory addresses = new address[](donationsForCause.length);
+        uint[] memory donationAmounts = new uint[](donationsForCause.length);
+        uint[] memory donationTimestamps = new uint[](donationsForCause.length);
+
+        for (uint256 i = 0; i < donationsForCause.length; i++) {
+            addresses[i] = donationsForCause[i].donator;
+            donationAmounts[i] = donationsForCause[i].amount;
+            donationTimestamps[i] = donationsForCause[i].timestamp;
+        }
+
+        return (addresses, donationAmounts, donationTimestamps);
+    }
 
 
-    function isCauseRegistered(string memory description) private view returns (bool) {
+    function isCauseRegistered(string memory title) private view returns (bool) {
         for (uint256 i = 0; i < registeredCauses.length; i++) {
-            if (strCompare(registeredCauses[i], description)) {
+            if (strCompare(registeredCauses[i], title)) {
                 return true;
             }
         }
 
         return false;
     }
-    
-    function isCauseFinished(string memory description) private view returns (bool) {
-        for (uint256 i = 0; i < registeredCauses.length; i++) {
-            if (strCompare(finishedCauses[i], description)) {
+
+    function isCauseFinished(string memory title) private view returns (bool) {
+        for (uint256 i = 0; i < finishedCauses.length; i++) {
+            if (strCompare(finishedCauses[i], title)) {
                 return true;
             }
         }
 
         return false;
     }
-    
+
     function strCompare(string memory left, string memory right) private pure returns (bool) {
-      return keccak256(abi.encodePacked(left)) == keccak256(abi.encodePacked(right));
+        return keccak256(abi.encodePacked(left)) == keccak256(abi.encodePacked(right));
     }
 
 
@@ -101,7 +125,4 @@ contract ChainFund {
 
         cause.beneficiary.transfer(amount);
     }
-
-
-
 }
--- chainfund/Donation.sol
+++ chainfund/Donation.sol
@@ -2,6 +2,6 @@ pragma solidity >=0.6.6 <0.8.0;
 
 struct Donation {
     address donator;
-    uint256 ammount;
+    uint256 amount;
     uint256 timestamp;
 }
